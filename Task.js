02d75feda59cea07a143b47099fb7e90f518c5c0
Andrzej Stasinski <andis_as@wp.pl>
2021-02-17 18:56:31 +0100

    task and buttons - not active

--- Task.js
+++ Task.js
@@ -1,5 +1,32 @@
 
+class Task {
+    constructor(task, onDone, onDelete) {
+        this.task = task
+        this.onDone = onDone
+        this.onDelete = onDelete
+    }
+    render() {
+        const container = document.createElement('div')
+        const p = document.createElement('p')
+        p.textContent = this.task.task
 
+        const buttonDone = new Button('Done', this.onDone)
+        const buttonDelete = new Button('Delete', this.onDelete)
+        
+        container.appendChild(p)
+        if(this.task.done) {
+            p.classList.add('task-done')
+        }
+
+        const buttons = document.createElement('div')
+        buttons.appendChild(buttonDone.render())
+        buttons.appendChild(buttonDelete.render())
+
+        container.appendChild(buttons)
+        container.classList.add('containerTask')
+        return container        
+    }
+}
 
 
 
