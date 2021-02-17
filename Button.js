70281bb7ed38d8846716bc5fe1e9f8b741547b91
Andrzej Stasinski <andis_as@wp.pl>
2021-02-17 17:36:46 +0100

    Initial app

---
+++ Button.js
@@ -0,0 +1,123 @@
+
+class Button {
+    constructor(label, onClick) {
+        this.label = label
+        this.onClick = onClick
+    }
+    render() {
+        const button = document.createElement('button')
+        button.textContent = this.label
+        button.addEventListener('click', this.onClick)
+        return button
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
